9f17ba2eee9adda61692541aebb58d90cd883331
Jishan Shaikh <jshaikh@digite.com>
2021-02-01 10:06:04 +0530

    Fixed current failing test cases

--- src/main/java/com/digite/kata/ParkingLot.java
+++ src/main/java/com/digite/kata/ParkingLot.java
@@ -2,6 +2,7 @@ package com.digite.kata;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ParkingLot {
@@ -21,8 +22,31 @@ public class ParkingLot {
     }
 
     public void park(String registrationNumber, String colour) {
-        this.slots.put(1, new Car(registrationNumber, colour));
-        this.currentCapacity -= 1;
+        if ( currentCapacity > 0 ) {
+            this.slots.put(getNearestVacantSlot(), new Car(registrationNumber, colour));
+            this.currentCapacity -= 1;
+        }
+    }
+
+    public int getNearestVacantSlot() {
+        Integer vacantSlot = -1;
+
+        Set<Integer> keys = slots.keySet();
+        int counter = 0;
+
+        while ( counter < slots.size() ) {
+            counter++;
+
+            if ( !slots.containsKey(counter) ) {
+                vacantSlot = counter;
+                break;
+            }
+        }
+
+        if ( vacantSlot == -1 )
+            vacantSlot = slots.size() + 1;
+
+        return vacantSlot;
     }
 
     public String getColourByRegistrationNumber(String registrationNumber) {
@@ -31,11 +55,20 @@ public class ParkingLot {
                 .collect(Collectors.joining());
     }
 
-    public void leave(int i) {
-
+    public void leave(int slot) {
+        slots.remove(slot);
+        currentCapacity += 1;
     }
 
-    public int getSlotNumberByRegistrationNumber(String s) {
-        return 0;
+    public int getSlotNumberByRegistrationNumber(String registrationNumber) {
+        int slot = -1;
+
+        for (Map.Entry<Integer, Car> entry : slots.entrySet()) {
+            if (entry.getValue().getRegistrationNumber().equals(registrationNumber)) {
+                slot = entry.getKey();
+            }
+        }
+
+        return slot;
     }
 }
